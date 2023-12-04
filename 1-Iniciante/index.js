7880c7229494dafb2a724447e5ee5cfbf5250527
Dyovana <dyo.menegatti@hotmail.com>
2023-12-04 08:51:15 -0300

    Finaliza primeiro modulo - secao 2: js basico: para iniciantes

---
+++ 1-Iniciante/index.js
@@ -0,0 +1,30 @@
+// // Aula 21. alert, confirm e prompt
+// let num1 = prompt('Digite um número:');
+// let num2 = prompt('Digite outro número:');
+
+// num1 = Number(num1);
+// num2 = Number(num2);
+
+// let soma = num1 + num2;
+
+// alert(`A soma dos números é: ${soma}`);
+
+// // Aula 23. Variáveis
+// let varA = 'A'; // B
+// let varB = 'B'; // C
+// let varC = 'C'; // A
+
+// console.log(`Original- A: ${varA} - B: ${varB} - C: ${varC}`);
+
+// const varATempo = varA;
+// varA = varB;
+// varB = varC;
+// varC = varATempo;
+
+// console.log(`A: ${varA} - B: ${varB} - C: ${varC}`);
+
+// Aula 27. Objeto Math
+let num1 = 9.54578;
+let num2 = Math.floor(num1);
+
+console.log(num2);
